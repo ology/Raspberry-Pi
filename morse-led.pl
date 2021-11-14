cd226e4ecb5d00fe6e81c42639014de75163a7e9
Gene Boggs <gene@ology.net>
2021-11-13 18:12:14 -0800

    First project!

---
+++ morse-led.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Convert::Morse qw( as_morse );
+use HiPi qw( :rpi );
+use HiPi::GPIO;
+use IO::Prompt::Tiny 'prompt';
+use Time::HiRes qw( usleep );
+
+my $message = shift || die "Usage: perl $0 'Some message'\n";
+
+my $morse = as_morse( $message );
+$morse =~ s/\s+/ /g;
+print "'$message => $morse\n";
+
+my $gpio = HiPi::GPIO->new;
+
+my $pin = $gpio->get_pin( RPI_PIN_11 );
+$pin->mode( RPI_MODE_OUTPUT );
+
+#$pin->value( RPI_LOW ); exit; # Oops!
+
+while ( 1 ) {
+  my $response = prompt( 'Enter=go q=quit', 'Enter' );
+
+  if ( $response eq 'q' ) {
+    last;
+  }
+
+  for my $char ( split '', $morse ) {
+    if ( $char eq ' ' ) {
+      sleep 2;
+      next;
+    }
+
+    $pin->value( RPI_HIGH );
+
+    if ( $char eq '.' ) {
+      usleep 300_000;
+    }
+    elsif ( $char eq '-' ) {
+      sleep 1;
+    }
+
+    $pin->value( RPI_LOW );
+
+    usleep 500_000;
+  }
+}
