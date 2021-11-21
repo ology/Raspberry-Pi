e5ca1c1b632bdde5fb64515bb206a412e246e792
Gene Boggs <gene@ology.net>
2021-11-21 05:24:36 -0800

    Port from HiPi to WiringPi

---
+++ button-led-wp.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# The wiring is that of the second project in the
+# "Freenove Ultimate Starter Kit" at https://freenove.com/tutorial.html
+
+use RPi::WiringPi;
+use RPi::Const qw(:all);
+
+my $led_pin_num = shift || 11; # 1 .. 40
+my $btn_pin_num = shift || 12; # "
+
+my $pi = RPi::WiringPi->new;
+
+my $led_pin = $pi->pin( $led_pin_num );
+my $btn_pin = $pi->pin( $btn_pin_num );
+
+$led_pin->mode( OUTPUT );
+$btn_pin->mode( INPUT );
+$btn_pin->pull( PUD_UP );
+
+while ( 1 ) {
+  my $btn_level = $btn_pin->read;
+
+  if ( $btn_level == LOW ) {
+    $led_pin->write( HIGH );
+    print "LED on...\n";
+  }
+  else {
+    $led_pin->write( LOW );
+    print "LED off...\n";
+  }
+}
