d0826fba4405249f403fa83fd17bc338af4f9020
Gene Boggs <gene@ology.net>
2021-11-28 10:23:57 -0800

    Improve var name

--- multicolor-led-wp.pl
+++ multicolor-led-wp.pl
@@ -13,19 +13,19 @@ use IO::Async::Loop;
 use RPi::WiringPi;
 use RPi::Const qw(:all);
 
-my $led_pin_num_red   = shift || 17; # GPIO numbering
-my $led_pin_num_green = shift || 18; # "
-my $led_pin_num_blue  = shift || 27; # "
+my $pin_num_red   = shift || 17; # GPIO numbering
+my $pin_num_green = shift || 18; # "
+my $pin_num_blue  = shift || 27; # "
 
 my $pi = RPi::WiringPi->new;
 
-my $led_pin_red   = $pi->pin( $led_pin_num_red );
-my $led_pin_green = $pi->pin( $led_pin_num_green );
-my $led_pin_blue  = $pi->pin( $led_pin_num_blue );
+my $pin_red   = $pi->pin( $pin_num_red );
+my $pin_green = $pi->pin( $pin_num_green );
+my $pin_blue  = $pi->pin( $pin_num_blue );
 
-$led_pin_red->mode( PWM_OUT );
-$led_pin_green->mode( PWM_OUT );
-$led_pin_blue->mode( PWM_OUT );
+$pin_red->mode( PWM_OUT );
+$pin_green->mode( PWM_OUT );
+$pin_blue->mode( PWM_OUT );
 
 my $loop = IO::Async::Loop->new;
 
@@ -43,7 +43,7 @@ sub multi {
   my $g = int rand 101;
   my $b = int rand 101;
 
-  $led_pin_red->pwm( $r );
-  $led_pin_green->pwm( $g );
-  $led_pin_blue->pwm( $b );
+  $pin_red->pwm( $r );
+  $pin_green->pwm( $g );
+  $pin_blue->pwm( $b );
 }
