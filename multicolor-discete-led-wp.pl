7e6961dee7b0d7fb657dd21ba6284de510c60090
Gene Boggs <gene@ology.net>
2021-11-28 14:37:35 -0800

    Initial commit

---
+++ multicolor-discete-led-wp.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# The wiring is that of project 5.1 in the
+# "Freenove Ultimate Starter Kit" at
+#  https://freenove.com/tutorial.html
+
+use IO::Async::Timer::Periodic;
+use IO::Async::Loop;
+use RPi::WiringPi;
+use RPi::Const qw(:all);
+
+my $pin_num_red   = shift || 17; # GPIO numbering
+my $pin_num_green = shift || 18; # "
+my $pin_num_blue  = shift || 27; # "
+
+my $pi = RPi::WiringPi->new;
+
+my $pin_red   = $pi->pin( $pin_num_red );
+my $pin_green = $pi->pin( $pin_num_green );
+my $pin_blue  = $pi->pin( $pin_num_blue );
+
+$pin_red->mode( OUTPUT );
+$pin_green->mode( OUTPUT );
+$pin_blue->mode( OUTPUT );
+
+my $loop = IO::Async::Loop->new;
+
+my $timer = IO::Async::Timer::Periodic->new(
+   interval => 1,
+   on_tick  => \&multi,
+);
+
+$timer->start;
+$loop->add( $timer );
+$loop->run;
+
+sub multi {
+  my $pin = ( $pin_red, $pin_green, $pin_blue )[ int rand 3 ];
+  $pin->write( not $pin->read );
+}
